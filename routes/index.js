955361d1c7c38b8599f4352c654ddb9fb8c21f7e
anasauce <anasauceda1843@gmail.com>
2016-08-30 14:16:42 -0700

    Query and logic to see all boiks in db

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-// var queryLogic = require('../queryLogic')
+var queryLogic = require('../queryLogic')
 
 router.get('/', (request, response) => {
-  response.send( { data: 'hi' })
-  // queryLogic.getEverything()
-  // .then( data => res.json(data) )
-  // .catch( error => response.send({ error, message: error.message }))
+  //response.send( { data: 'hi' })
+  queryLogic.getEverything()
+  .then( data => response.json(data) )
+  .catch( error => response.send({ error, message: error.message }))
 })
 
 // router.get('/test', (req, res) => {
